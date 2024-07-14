f3a865976964af3283900ad3e0eb1dc7a2b9bdbf
inbask2003 <inbask2003@gmail.com>
2024-07-14 14:34:48 +0530

    ListNode.java Added

---
+++ ListNode.java
@@ -0,0 +1,20 @@
+public class ListNode
+{
+    int val;
+    ListNode next;
+
+    ListNode()
+    {
+
+    }
+
+    ListNode(int val)
+    {
+
+    }
+
+    ListNode(int val,ListNode next)
+    {
+
+    }
+}
