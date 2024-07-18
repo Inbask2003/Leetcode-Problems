b8e5dac99efe92d477ec0b3b3b8c74204f0f3482
inbask2003 <inbask2003@gmail.com>
2024-07-18 07:35:00 +0530

    Leetcode_392.java Added

---
+++ Leetcode_392.java
@@ -0,0 +1,61 @@
+/*
+  392. Is Subsequence --> (18/07/2024) --> (Easy)
+
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+
+Constraints:
+
+0 <= s.length <= 100
+0 <= t.length <= 104
+s and t consist only of lowercase English letters.
+ */
+
+public class Leetcode_392
+{
+    public static void main(String[] args)
+    {
+        String s = "abc";
+        String t = "ahbgdc";
+        System.out.println(isSubsequence(s,t));
+    }
+
+    public static boolean isSubsequence(String s, String t)
+    {
+        int sLen = s.length();
+        int tLen = t.length();
+        if (sLen > tLen)
+        {
+            return false;
+        }
+        int i = 0;
+        int j = 0;
+        int total = 0;
+        while ((i < sLen) && (j < tLen)) {
+            if(s.charAt(i) == t.charAt(j))
+            {
+                total++;
+                i++;
+                j++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+
+        return total == sLen;
+
+    }
+}
